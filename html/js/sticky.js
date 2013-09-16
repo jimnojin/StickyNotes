5c240e16e85749fa63eb4580f6eb1570be499e31
jimnojin <niki.markov@gmail.com>
2013-09-16 18:39:28 +0300

    defaults in object

--- html/js/sticky.js
+++ html/js/sticky.js
@@ -1,15 +1,24 @@
 ;(function(ns) {
     var noteStyles = ['blue', 'yellow', 'pink'];
     
-	function Note(config) {
-	    config = config || {};
-		this.id = config.id || '_' + (Math.random().toString(36).substr(2, 9)); // ID or pseudo random ID
-		this.x = config.x || App.container.position().left + 20;
-		this.y = config.y || App.container.position().top + 20;
-		this.title = config.title || '' ;
-		this.text =config.text || '';
-		this.style = config.style || noteStyles[Math.round(Math.random() * (noteStyles.length - 1))];
-		this.minimized = !!config.minimized || false;
+	function Note(cfg) {
+	    var config = $.extend({
+	    	id : '_' + (Math.random().toString(36).substr(2, 9)), //  pseudo random ID
+	    	x: App.container.position().left + 20,
+	    	y: App.container.position().top + 20,
+	    	title: '',
+	    	text: '',
+	    	style: noteStyles[Math.round(Math.random() * (noteStyles.length - 1))],
+	    	minimized: false
+	    }, cfg);
+	    
+		this.id = config.id;
+		this.x = config.x;
+		this.y = config.y;
+		this.title = config.title;
+		this.text =config.text;
+		this.style = config.style;
+		this.minimized = config.minimized;
 	}
 	
 	Note.prototype = {
@@ -26,36 +35,36 @@
 		        }
 		    } else {
 		        var rotation = Math.round(Math.random() * 20 - 10); // random angle (-10..10)deg rotation
-    		    var html = ['<div class="note ', this.style, '" id="', this.id, '" data-state="max" style="display: none; top: ', this.y, 'px; left: ', this.x, 'px;  -webkit-transform: rotate(', rotation, 'deg);">',
-            				    '<div class="header">',
-            				        '<div class="pin"></div>',
-            					    '<div class="mintitle"></div>',
-            				    '</div>',
-            				    '<div class="title">', this.title, '</div>',
-            				    '<div class="text">', this.text, '</div>',
-            			    '</div>'
-            	    	].join('');
-            	App.container.append(html);
-            	elem = $('#' + this.id);
-            	
-            	elem.fadeIn('fast');
-        	    elem.draggable({
-            	    containment: "parent",
-              	    drag: function(e, ui) {
-            	        that.x = ui.offset.left;
-            	        that.y = ui.offset.top;
-            	    }
-                });
+	    	    var html = ['<div class="note ', this.style, '" id="', this.id, '" data-state="max" style="display: none; top: ', this.y, 'px; left: ', this.x, 'px;  -webkit-transform: rotate(', rotation, 'deg);">',
+	    				    '<div class="header">',
+	    				        '<div class="pin"></div>',
+	    					    '<div class="mintitle"></div>',
+	    				    '</div>',
+	    				    '<div class="title">', this.title, '</div>',
+	    				    '<div class="text">', this.text, '</div>',
+	    			    '</div>'
+	    	    	].join('');
+	    	App.container.append(html);
+	    	elem = $('#' + this.id);
+	    	
+	    	elem.fadeIn('fast');
+		    elem.draggable({
+	    	    containment: "parent",
+	      	    drag: function(e, ui) {
+	    	        that.x = ui.offset.left;
+	    	        that.y = ui.offset.top;
+	    	    }
+	        });
 		    }
 		    if (this.minimized) {
-        	    elem.addClass('minimized', 'fast');
-        	    $('.title, .text', elem).hide();
-        	    $('.mintitle', elem).html(this.title);
-        	} else {
-        	    elem.removeClass('minimized', 'fast');
-        	    $('.mintitle', elem).html('');
-        	    $('.title, .text', elem).show();
-        	}
+		    elem.addClass('minimized', 'fast');
+		    $('.title, .text', elem).hide();
+		    $('.mintitle', elem).html(this.title);
+		} else {
+		    elem.removeClass('minimized', 'fast');
+		    $('.mintitle', elem).html('');
+		    $('.title, .text', elem).show();
+		}
 		},
 		remove: function() {
 		    $('#' + this.id).fadeOut('fast', function() { $(this).remove() });
